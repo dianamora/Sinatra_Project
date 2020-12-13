545e99002ae8f8f574b3fffa2076c9ad77cd1f24
Diana Mora <morra.dianna@gmail.com>
2020-12-13 18:52:56 -0500

    Added Logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -21,19 +21,40 @@ class UserController < ApplicationController
     get '/users/login' do
         #if user is logged in, redirect to their homepage
         #else, show them the login form
+    if !logged_in?
         erb :'/users/login'
+    else 
+        @user = User.find(session[:user_id])
+        redirect "/users/#{@user.id}"
+    end
     end
 
-    post '/users/login' do
-        #first we want to find the user if it exists
-        #if user exists, then we want to authenticate password
+    post '/users/login' do #first we want to find the user if it exists #if user exists, then we want to authenticate password
         @user = User.find_by(username: params[:username])
 
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/users/#{@user.id}"
+        else
+            redirect '/users/login'
+        end
     end
 
     get '/users/:id' do 
         @user = User.find(params[:id])
          erb :'users/show'
     end
+
+    get '/users/logout' do 
+        session.clear
+        redirect '/'
+    end 
+    
+    post '/users/logout' do
+        session.clear
+        redirect '/'
+      end
+
+      
 
 end
